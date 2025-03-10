cb0d6788c00061e521f1617ea2b92b3e8ecbf006
osivff <oscar.fitchfierro@gmail.com>
2025-03-10 10:54:21 -0700

    My solution to reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newStr = "";
+    let lastChar = str.length - 1;
+    while(lastChar >= 0){
+        newStr += str[lastChar];
+        lastChar--;
+    }
+    return newStr;
 };
 
 // Do not edit below this line
