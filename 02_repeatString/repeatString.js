dfbae3a4339836b75288ec661e39cfdc86987cf1
osivff <oscar.fitchfierro@gmail.com>
2025-03-10 10:42:14 -0700

    My solution to repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+    let newStr = "";
+    if(count < 0){
+        return 'ERROR';
+    } else {
+        for(let i = 0; i < count; i++){
+            newStr += str;
+        }
+        return newStr;
+    }
 };
 
 // Do not edit below this line
