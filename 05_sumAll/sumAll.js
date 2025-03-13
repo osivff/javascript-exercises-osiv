7003cd551a9a9e52c0c654be470b4aac3d1ba5ef
osivff <oscar.fitchfierro@gmail.com>
2025-03-13 06:28:32 -0700

    Solution to sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    let checkIntegerA = Number.isInteger(a);
+    let checkIntegerB = Number.isInteger(b);
+    let checkTypeOfA = typeof(a);
+    let checkTypeOfB = typeof(b);
 
-};
+    if(a < 0 || b < 0 || !checkIntegerA || !checkIntegerB || checkTypeOfA != "number" || checkTypeOfB != "number"){
+        return 'ERROR';
+    }else if(a < b){
+        for(a; a <= b; a++){
+            sum += a;
+        }
+    } else {
+        for(b; b <= a; b++){
+            sum += b;
+        }
+    }
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
