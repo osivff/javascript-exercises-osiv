e3d06339af0867d7b46bd48e3281ea86dbea8bd4
osivff <oscar.fitchfierro@gmail.com>
2025-03-14 05:12:01 -0700

    Solution to leapYear

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    isLeapYear = year % 4;
+    isException = year % 400;
+    isNotLeapYear = year % 100;
 
+    if(isException === 0 && isLeapYear === 0){
+        return true;
+    } else if(isLeapYear === 0 && isNotLeapYear != 0){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
