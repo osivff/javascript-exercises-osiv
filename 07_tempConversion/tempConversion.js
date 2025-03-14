7f7f895acd8becdd06591aa2d78c7253c5476013
osivff <oscar.fitchfierro@gmail.com>
2025-03-14 05:32:00 -0700

    Solution to tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let isCelsius;
+  isCelsius = (temp - 32) * (5/9);
+  return Number(isCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let isFahrenheit = 0;
+  isFahrenheit = (temp * (9/5)) + 32;
+  return Number(isFahrenheit.toFixed(1)); 
 };
 
 // Do not edit below this line
