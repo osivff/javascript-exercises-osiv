c8372a9c8f1128158a52f7e999977276dcb4e985
osivff <oscar.fitchfierro@gmail.com>
2025-04-18 05:51:17 -0700

    All tests included

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (Number(num) < 0) return "OOPS";
+    if (Number(num) === 0) return 0;
+    if (Number(num) === 1) return 1;
+    return fibonacci(Number(num) - 1) + fibonacci(Number(num) - 2);
 };
 
 // Do not edit below this line
