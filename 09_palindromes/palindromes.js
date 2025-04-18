23f71b0b06045afed682cee79a3023bb6b7f0350
osivff <oscar.fitchfierro@gmail.com>
2025-04-18 04:58:15 -0700

    Palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let arr = str.split('');
+    let newArr = [];
+    let revArr = [];
+    console.log(arr);
+    arr.forEach((current) => {
+      if(/^[a-zA-Z0-9]$/.test(current)){
+          newArr.push(current);
+      }
+    })
+    
+    for(i = newArr.length -1; i >= 0; i--){
+      revArr.push(newArr[i]);
+    }
+    for(i = 0; i <= newArr.length -1; i++){
+      if(newArr[i].toLowerCase() !== revArr[i].toLowerCase()){
+        return false;
+      }
+    }
+    return true;
 };
 
 // Do not edit below this line
