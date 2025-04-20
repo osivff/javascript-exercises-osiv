df64f8435fa67b5b84bfcfccce92849783f34f33
osivff <oscar.fitchfierro@gmail.com>
2025-04-19 18:36:00 -0700

    Include all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  let oldest = arr.reduce((accumulator, currentValue) => {
+    let currentDeath = currentValue.yearOfDeath || 2025;
+    let accumulatorDeath = accumulator.yearOfDeath || 2025;
+    
+    let currentAge = accumulatorDeath - accumulator.yearOfBirth;
+    let nextPersonAge = currentDeath - currentValue.yearOfBirth;
+    
+    if(currentAge < nextPersonAge){
+      return currentValue;
+    } else {
+      return accumulator;
+    }
+  });
+  return oldest;
 };
 
 // Do not edit below this line
